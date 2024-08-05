a6837b1e06d56a2c0ebfdefc47278ac43da4e41a
user.name <gmail@hdknr.com>
2024-08-05 20:26:24 +0900

    ruff

--- bedrag/models/models.py
+++ bedrag/models/models.py
@@ -18,18 +18,14 @@ class BedrockKnowlegeBase(Base):
 
     __abstract__ = True
 
-    id: Mapped[UUID] = mapped_column(
-        UUID, primary_key=True, comment="primary_key_field"
-    )
+    id: Mapped[UUID] = mapped_column(UUID, primary_key=True, comment="primary_key_field")
     embedding: Mapped[Vector] = mapped_column(Vector(1536), comment="vector_field")
     bedrock_meta: Mapped[JSON] = mapped_column(JSON, comment="metadata_field")
     chunks: Mapped[str] = mapped_column(Text, comment="text_field")
 
     @classmethod
     def get_vector_field(cls) -> Column:
-        return next(
-            filter(lambda i: i.comment == "vector_field", cls.__table__.columns)
-        )
+        return next(filter(lambda i: i.comment == "vector_field", cls.__table__.columns))
 
     @classmethod
     def get_field_mapping(cls) -> Column:
