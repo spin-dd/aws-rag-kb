1032d60759cf6494ed00bc0773121190624fc935
user.name <gmail@hdknr.com>
2024-08-05 09:36:01 +0900

    環境変数のデータを元に database 情報を用意する

--- tofu/rag/variables.tf
+++ tofu/rag/variables.tf
@@ -16,11 +16,28 @@ variable "region" {
 
 }
 
+variable "database_table_name" {
+  type    = string
+  default = "rag.bedrock.kb"
+}
+variable "database_user" {
+  type = string
+}
+variable "database_master_user" {
+  type = string
+}
+
 locals {
   region = var.region
   symbol = {
     service = var.service
     env     = var.env
     prefix  = "${var.service}-${var.env}"
+  }
+
+  database = {
+    name        = regex("^(?P<db>[^\\.]+).(?P<schema>[^\\.]+).(?P<table>[^\\.]+)$", var.database_table_name)
+    user        = regex("^(?P<username>[^.]+)\\s(?P<password>[^.]+)$", var.database_user)
+    master_user = regex("^(?P<username>[^.]+)\\s(?P<password>[^.]+)$", var.database_master_user)
   }
 }
