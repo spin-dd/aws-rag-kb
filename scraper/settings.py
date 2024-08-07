5842f4e2504a89808ed59d190fd91573f46d770d
user.name <gmail@hdknr.com>
2024-08-07 11:19:23 +0900

    scrapy 重複処理させない

--- scraper/settings.py
+++ scraper/settings.py
@@ -91,3 +91,5 @@ ITEM_PIPELINES = {
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
+
+DUPEFILTER_CLASS = "scrapy.dupefilters.BaseDupeFilter"
