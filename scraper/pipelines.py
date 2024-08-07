4783e303245c66f767bec513c5f174dae48fbbcb
user.name <gmail@hdknr.com>
2024-08-07 11:19:02 +0900

    KBデータとメタデータを指定

--- scraper/pipelines.py
+++ scraper/pipelines.py
@@ -7,7 +7,11 @@
 # useful for handling different item types with a single interface
 # from itemadapter import ItemAdapter
 
+from bedrag.s3 import put_text
+from .items import RagSourceItem
 
 class RagSoourcePipeline:
-    def process_item(self, item, spider):
+    def process_item(self, item: RagSourceItem, spider):
+        put_text(item.text, item.bucket, item.path)
+        put_text(item.meta, item.bucket, item.path + ".json")
         return item
